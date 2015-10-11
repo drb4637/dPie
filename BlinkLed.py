febc5ed52ca81f34ef12a2b9f0f6b2b315383b3f
drb4637 <drb4637@gmail.com>
2015-10-11 18:59:40 -0400

    added simple led blink thingy

---
+++ BlinkLed.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+#blink logic
+def blink(pin)
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(pin,GPIO.LOW)
+    time.sleep(1)
+    return
+
+#set the board
+GPIO.setmode(GPIO.BOARD)
+
+#set the output channel
+GPIO.setup(6, GPIO.OUT)
+
+for i in range(0,50):
+    blink(6)
+GPIO.cleanup()
